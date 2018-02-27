942f53ebff1ecc28119a55b8fdf84c7ec4020dcd
Sayan Goswami <goswami.sayan47@gmail.com>
2018-02-28 00:37:15 +0530

    Add named pipe

---
+++ named_pipe/consumer.py
@@ -0,0 +1,16 @@
+import json, os, tempfile
+
+filename = open("config.cfg", "r").readline()
+
+with os.open(filename, os.O_RDONLY) as pipe_in:
+    while True:
+        line = pipe_in.readline()
+
+        if line is None:
+            exit(-1)
+
+        line = line.decode('utf-8')
+        print(json.load(line))
+
+os.remove(filename)
+os.rmdir(tmpdir)
---
+++ named_pipe/producer.py
@@ -0,0 +1,25 @@
+import json, os, tempfile
+
+
+pipename = "ipc_pipe"
+tmpdir = tempfile.mkdtemp()
+filename = os.path.join(tmpdir, pipename)
+
+with open("config.cfg", "w") as config:
+    config.write(filename)
+
+try:
+    os.mkfifo(filename)
+except Exception:
+    print("Exception")
+    os.remove(filename)
+    os.mkfifo(filename)
+
+
+pipe_out = os.open(filename, os.O_WRONLY)
+
+for i in range(1000):
+    data = json.dumps({"num": i}).encode('utf-8')
+    os.write(pipe_out, data) 
+
+os.close(pipe_out)
